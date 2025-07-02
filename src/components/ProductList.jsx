a74e8bb924666fed02cd5454310389261c33951a
micheledidonna01 <micheledidonna337@gmail.com>
2025-07-02 10:39:00 +0200

    feat: bonus1

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+
 import { useReducer } from 'react';
 const ProductList = () => {
     // Stato iniziale
@@ -29,23 +29,22 @@ const ProductList = () => {
                 isProductInCart = products.items.some((addP) => addP.name === action.payload.name);
                 console.log(isProductInCart);
 
-                if (isProductInCart) {
-                    action.payload.quantity ++; // Incrementa la quantità del prodotto
-                    
+                if (isProductInCart) { // Incrementa la quantità del prodotto
+
                     return {
                         ...products,
                         items: products.items.map(prod =>
                             prod.name === action.payload.name
-                                ? {...prod, quantity: prod.quantity + 1} : prod
+                                ? { ...prod, quantity: prod.quantity + 1 } : prod
                         ),
                         total: products.total + action.payload.price
 
                     };
-                
-                }else{
-                    return{
+
+                } else {
+                    return {
                         ...products,
-                        items: [...products.items, {...action.payload, quantity: 1}],
+                        items: [...products.items, { ...action.payload, quantity: 1 }],
                         total: products.total + action.payload.price
                     }
                 }
@@ -56,57 +55,58 @@ const ProductList = () => {
                     items: products.items.filter((_, i) => i !== action.payload),
                     total: products.total - (productToRemove.price * productToRemove.quantity)
                 }
-                default:
-                    return products;
-                // console.log("Prodotto aggiunto al carrelo: ", action.payload);
-                // setAddedProducts([...products, { ...action.payload, quantity: 1 }]);
-                // console.log(addedProducts);
-                // console.log(action.payload);
-                // if(action.payload.quantity >= 1){
-                //     return {...action.payload,
-                //             quantity: 1
-                //         }
-                // }
-                // if(action.payload.quantity === 0){
-                //     const addProdToCart = {
-                //         ...action.payload,
-                //         quantity: 1, // Imposta la quantità iniziale a 1
-                //     }
-                //     console.log(addProdToCart);
-                //     setAddedProducts([...addedProducts, addProdToCart]);
-                // }
-                // return setAddedProducts([...addedProducts, action.payload]);
-            
+            case 'UPDATE_QUANTITY':
+                return {
+                    ...products,
+                    items: products.items.map((p, i) => {
+                        if (i === action.payload.index) {
+                            
+                            return { ...p, quantity: action.payload.quantity }
+                        }
+                        return p;
+                    }),
+                    total: products.items.reduce((acc, value) => {
+                        console.log(value);
+                        if( value.name === action.payload.name){
+                            return acc + (value.price * action.payload.quantity);
+                        }
+                        return acc + value.price * value.quantity;
+                    }, 0)
+                }
+            default:
+                return products;
+            // console.log("Prodotto aggiunto al carrelo: ", action.payload);
+            // setAddedProducts([...products, { ...action.payload, quantity: 1 }]);
+            // console.log(addedProducts);
+            // console.log(action.payload);
+            // if(action.payload.quantity >= 1){
+            //     return {...action.payload,
+            //             quantity: 1
+            //         }
+            // }
+            // if(action.payload.quantity === 0){
+            //     const addProdToCart = {
+            //         ...action.payload,
+            //         quantity: 1, // Imposta la quantità iniziale a 1
+            //     }
+            //     console.log(addProdToCart);
+            //     setAddedProducts([...addedProducts, addProdToCart]);
+            // }
+            // return setAddedProducts([...addedProducts, action.payload]);
 
-                // const addProdToCart = {
-                //     ...action.payload,
-                //     quantity: 1, // Imposta la quantità iniziale a 1
-                // }
-                // setAddedProducts([...addedProducts, addProdToCart]);
+
+            // const addProdToCart = {
+            //     ...action.payload,
+            //     quantity: 1, // Imposta la quantità iniziale a 1
+            // }
+            // setAddedProducts([...addedProducts, addProdToCart]);
         }
     }
     const [products, dispatchProducts] = useReducer(productsReducer, initialProducts);
-    
-    // const addToCart = (product) => {
-    //     const isProductInCart = addedProducts.some((addP) => addP.name === product.name);
-    //     console.log(isProductInCart);
 
-    //     if (isProductInCart) {
-    //         product.quantity += 1; // Incrementa la quantità del prodotto
-    //         console.log(product);
-    //         return product;
-    //     }
-    //     console.log("Prodotto aggiunto al carrelo: ", product);
-    //     setAddedProducts([...addedProducts, { ...product, quantity: 1 }]);
-    //     console.log(addedProducts);
-    // }
-
-
-
-
-
-
-    
+      const updateQuantity = (index, quantity, name) => {
+          dispatchProducts({ type: 'UPDATE_QUANTITY', payload: {index, quantity, name} })
+      };
 
     return <>
         <div className="list-product">
@@ -117,12 +117,12 @@ const ProductList = () => {
                         <p>
                             {product.name} - €{product.price.toFixed(2)}
                         </p>
-                        <button onClick={() => dispatchProducts({ type: 'ADD_PRODUCT', payload: product})}>Aggiungi al carrello</button>
+                        <button onClick={() => dispatchProducts({ type: 'ADD_PRODUCT', payload: product })}>Aggiungi al carrello</button>
                     </li>
                 ))}
             </ul>
         </div>
-        {products.items.length !== 0 && 
+        {products.items.length !== 0 &&
             <div className='cart'>
                 <h2>Carrello</h2>
                 <ul>
@@ -131,14 +131,19 @@ const ProductList = () => {
                             <p>
                                 {product.name} - €{(product.price * product.quantity).toFixed(2)} - Quantità: {product.quantity}
                             </p>
-                            <button onClick={() => dispatchProducts({type: 'REMOVE_PRODUCT', payload: index})}>Rimuovi</button>
+                            <div>
+                                <button onClick={() => dispatchProducts({ type: 'REMOVE_PRODUCT', payload: index })}>Rimuovi</button>
+                            </div>
+                            <div>
+                                <input type="number" value={product.quantity} onChange={(e) => updateQuantity(index, parseInt(e.target.value), name)} min={1} max={20} />
+                            </div>
                         </li>
                     ))}
                 </ul>
 
                 <p>Totale: {products.total.toFixed(2)}€</p>
 
-            </div> 
+            </div>
         }
     </>
 }
